3be3f2932abcc954c17930ae4767d61fa1a29d49
Joel <jacksonspirit755@gmail.com>
2025-02-26 15:10:10 +0530

    Initial commit with .gitignore and .env

---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main async()
+{
+  await dotenv.load();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget
+{
+  @override
+  WeatherState createState() => WeatherState();
+}
+
+class WeatherState extends State<HomeScreen>
+{
+  final TextEditingController cityController = TextEditingController();
+  double? temperature;
+  int? humidity;
+  String? iconCode;
+  bool isLoading = false;
+  String errorMessage = '';
+
+  Future<void> getWeatherData(String city) async
+  {
+    final apiKey=dotenv.env['OPENWEATHER_API_KEY'];
+    final url = 'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric';
+
+    setState(()
+    {
+      isLoading =true;
+      errorMessage = '';
+    });
+
+    try
+    {
+      final response=await http.get(Uri.parse(url));
+
+      if(response.statusCode == 200)
+      {
+        final data =json.decode(response.body);
+
+        setState(()
+        {
+          temperature =data['main']['temp'].toDouble();
+          humidity=data['main']['humidity'];
+          iconCode=data['weather'][0]['icon'];
+          isLoading=false;
+        });
+      }
+      else
+      {
+        setState(()
+        {
+          isLoading=false;
+          errorMessage='Please enter a valid name for the city';
+        });
+      }
+    }
+    catch(e) {
+      setState(() {
+        isLoading = false;
+        errorMessage = 'Error,Plz try again later';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather App'),
+      ) ,
+      body: Padding(
+        padding:const EdgeInsets.all(15.0),
+        child : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: cityController,
+              decoration: InputDecoration(
+                labelText: 'Enter City',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height:20),
+
+            ElevatedButton(
+              onPressed: ()
+              {
+                if(cityController.text.isNotEmpty)
+                {
+                  getWeatherData(cityController.text);
+                }
+              },
+              child : Text('Get Weather'),
+            ),
+            SizedBox(height: 20),
+
+            if(errorMessage.isNotEmpty)
+              Text(
+                errorMessage,
+                style: TextStyle(color: Colors.red),
+              ),
+
+            if(isLoading)
+              CircularProgressIndicator(),
+
+            if(!isLoading && temperature!=null)...[
+              Text(
+                'Temperature: ${temperature?.toStringAsFixed(1)} degree Celsius',
+                style: TextStyle(fontSize:20),
+
+              ),
+
+              if(iconCode!=null)
+                Image.network(
+                  'https://openweathermap.org/img/wn/$iconCode@2x.png',
+                  width: 50,
+                  height: 50,
+
+                ),
+            ]
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
